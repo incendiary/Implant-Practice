a681686415d929eca5ee2372ba9820fe4345f6fa
incendiary <a.horsewood@googlemail.com>
2023-06-20 19:57:49 +0800

    Base Dinvoke example

--- Csharp/600 DInvoke/Base_Dinvoke/Base_Dinvoke/Program.cs
+++ Csharp/600 DInvoke/Base_Dinvoke/Base_Dinvoke/Program.cs
@@ -1,3 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using DInvoke.DynamicInvoke;
+using static Base_Dinvoke.Win32;
 
+/* Example on Windows 10.0.19045 N/a Build 19045 */
+
+namespace Base_Dinvoke
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // create startup info
+            var startupInfo = new Win32.Startupinfo();
+            startupInfo.cb = Marshal.SizeOf(startupInfo);
+
+
+            // Define the PROCESS_INFORMATION structure
+            ProcessInformation processInfo = new ProcessInformation();
+
+            // create process parameter list/array
+
+            object[] parametersList =
+            {
+                null,
+                @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+                IntPtr.Zero,
+                IntPtr.Zero,
+                false,
+                (uint)0,
+                IntPtr.Zero,
+                @"C:\Program Files (x86)\Microsoft\Edge\Application\",
+                startupInfo,
+                processInfo
+            };
+
+            // Dynamic invoke
+            var success = (bool)Generic.DynamicApiInvoke(
+                "kernel32.dll",
+                "CreateProcessW",
+                typeof(Win32.CreateProcessDelegate),
+                ref parametersList);
+
+            // fail and bail
+            if (!success)
+            {
+                Console.WriteLine($"[x] CreateProcessW failed: {Marshal.GetLastWin32Error()}");
+                return;
+            }
+
+            // print process info
+            Console.WriteLine("dwProcessId : {0}", processInfo.dwProcessId);
+            Console.WriteLine("dwThreadId  : {0}", processInfo.dwThreadId);
+            Console.WriteLine("hProcess    : 0x{0:X}", processInfo.hProcess);
+            Console.WriteLine("hThread     : 0x{0:X}", processInfo.hThread);
+
+            // close handles
+            Win32.CloseHandle(processInfo.hThread);
+            Win32.CloseHandle(processInfo.hProcess);
+        }
+    }
+}
--- Csharp/600 DInvoke/Base_Dinvoke/Base_Dinvoke/Win32.cs
+++ Csharp/600 DInvoke/Base_Dinvoke/Base_Dinvoke/Win32.cs
@@ -1,11 +1,60 @@
-﻿using System;
+﻿using System.Runtime.InteropServices;
+using DInvoke;
+
 namespace Base_Dinvoke
 {
-	public class Win32
-	{
-		public Win32()
-		{
-		}
-	}
-}
+    internal static class Win32
+    {
+        // [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
 
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode, SetLastError = true)]
+        // public static extern bool CreateProcessW(
+
+        public delegate bool CreateProcessDelegate(
+            string lpApplicationName,
+            string lpCommandLine,
+            IntPtr lpProcessAttributes,
+            IntPtr lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            ref Startupinfo lpStartupInfo,
+            out ProcessInformation lpProcessInformation);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Startupinfo
+        {
+            public int cb;
+            public IntPtr lpReserved;
+            public IntPtr lpDesktop;
+            public IntPtr lpTitle;
+            public int dwX;
+            public int dwY;
+            public int dwXSize;
+            public int dwYSize;
+            public int dwXCountChars;
+            public int dwYCountChars;
+            public int dwFillAttribute;
+            public int dwFlags;
+            public short wShowWindow;
+            public short cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ProcessInformation
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+    }
+}
