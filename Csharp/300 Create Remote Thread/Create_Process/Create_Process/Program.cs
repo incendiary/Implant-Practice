5b8d33e2d50c0ec7a99c8afd824f479136026ebb
incendiary <a.horsewood@googlemail.com>
2023-06-20 19:46:45 +0800

    Csharp Create Process

--- Csharp/300 Create Remote Thread/Create_Process/Create_Process/Program.cs
+++ Csharp/300 Create Remote Thread/Create_Process/Create_Process/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+using Create_Process;
 
+namespace Create_Process
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create startup info
+            var startupInfo = new Win32.STARTUPINFO();
+            startupInfo.cb = Marshal.SizeOf(startupInfo);
+
+            // create process
+            var success = Win32.CreateProcessW(
+                null,
+                @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+                IntPtr.Zero,
+                IntPtr.Zero,
+                false,
+                0,
+                IntPtr.Zero,
+                @"C:\Program Files (x86)\Microsoft\Edge\Application\",
+                ref startupInfo,
+                out var processInfo);
+
+            // bail if it failed
+            if (!success)
+            {
+                Console.WriteLine($"[x] CreateProcessW failed: {Marshal.GetLastWin32Error()}");
+                return;
+            }
+
+            // print process info
+            Console.WriteLine("dwProcessId : {0}", processInfo.dwProcessId);
+            Console.WriteLine("dwThreadId  : {0}", processInfo.dwThreadId);
+            Console.WriteLine("hProcess    : 0x{0:X}", processInfo.hProcess);
+            Console.WriteLine("hThread     : 0x{0:X}", processInfo.hThread);
+
+            // close handles
+            Win32.CloseHandle(processInfo.hThread);
+            Win32.CloseHandle(processInfo.hProcess);
+        }
+    }
+}
---
+++ Csharp/300 Create Remote Thread/Create_Process/Create_Process/Win32.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Create_Process
+{
+    internal static class Win32
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool CreateProcessW(
+            string lpApplicationName,
+            string lpCommandLine,
+            IntPtr lpProcessAttributes,
+            IntPtr lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            ref STARTUPINFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct STARTUPINFO
+        {
+            public int cb;
+            public IntPtr lpReserved;
+            public IntPtr lpDesktop;
+            public IntPtr lpTitle;
+            public int dwX;
+            public int dwY;
+            public int dwXSize;
+            public int dwYSize;
+            public int dwXCountChars;
+            public int dwYCountChars;
+            public int dwFillAttribute;
+            public int dwFlags;
+            public short wShowWindow;
+            public short cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+    }
+}
+
